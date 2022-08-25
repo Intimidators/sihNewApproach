544265c10be8fea768587d6e4b9b140dc74d35a8
unknown <nknittin16436@gmail.com>
2022-08-26 02:59:56 +0530

    Added redux store

--- src/portals/webinars/Webinars.jsx
+++ src/portals/webinars/Webinars.jsx
@@ -4,15 +4,12 @@ import "./webinars.css";
 import "./App.css";
 import HomePage from "./components/HomePage/HomePage";
 
-
 const Webinars = () => {
   return (
     <>
       <div className="webinars">
         <div className="webinars__container">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
+          <HomePage />
         </div>
       </div>
     </>
